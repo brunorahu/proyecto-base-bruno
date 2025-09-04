cd360773966f9c0c29aa0ad381accbe867c4627d
Bruno Ramos <193126-7@iberoleon.edu.mx>
2025-09-04 12:54:17 -0600

    Creación de archivos main.py, README.txt y avanzadas.py
    Se incorporó la función "potencia"

---
+++ main.py
@@ -0,0 +1,11 @@
+from clases.avanzadas import Operaciones
+
+def main():
+    op = Operaciones()
+    op.leerNumeros()
+    op.potencia()
+    op.raiz_cuadrada()
+    op.mostrarResultado()
+    
+if __name__ == "__main__":
+    main()
